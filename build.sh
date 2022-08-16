1bbefd03b6c05316d85778c8a12d47fc9ca84454
Tankred Ott <ott.tankred@gmail.com>
2022-08-16 19:00:05 +0200

    rework

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd pyckmeans-gui-frontend
 quasar build
 cd ..
-rsync -avu --delete pyckmeans-gui-frontend/dist/spa/ pyckmeans-gui-backend/pyckmeans_gui_backend/static/
+rsync -avu --delete pyckmeans-gui-frontend/dist/spa/ pyckmeans-gui-backend/pyckmeans_gui_backend/app/static/
